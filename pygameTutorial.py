3297e31985324bf410135becd2821cc042530d51
Joseph Moore <josephaldenmoore@gmail.com>
2018-08-17 17:35:29 -0400

    wire frame, animation

--- pygameTutorial.py
+++ pygameTutorial.py
@@ -8,10 +8,44 @@ vertexes = np.array([[-1,-1,-1],[-1,-1,1],
                      [1,-1,-1],[1,-1,1],
                      [1,1,-1],[1,1,1]])
 
+class Face:
+    def __init__(self, points, color):
+        self.points = points
+        self.color = color
+        self.updateZ()
+
+    def updateZ(self):
+        global vertexes
+        zsum = 0
+        for point in self.points:
+            zsum += vertexes[point][2]
+        self.z_val = zsum / len(vertexes)
+
+edges = [(0,1),
+         (0,2),
+         (1,3),
+         (2,3),
+         (0,4),
+         (1,5),
+         (2,6),
+         (3,7),
+         (4,5),
+         (4,6),
+         (5,7),
+         (6,7)]
+
+faces = [ Face([1, 5, 7, 3], [0,0,0]),
+          Face([2, 3, 7, 6], [255, 0, 0]),
+          Face([0, 2, 6, 4], [0, 255, 0]),
+          Face([0, 1, 5, 4], [0, 0, 255]),
+          Face([0, 1, 3, 2], [255, 255, 0]),
+          Face([4, 6, 7, 5], [255, 0, 255])]
+
+
 num_pts = len(vertexes)
 
 # List of Vertices, Angle -> List of Vertices
-# Performs a rotation transformation around the z-axis
+# Performs a rotation transformation around the Y-axis
 # returns the result of the operation
 def rotateY(verts, angle) :
     rot = np.array([[np.cos(angle), 0, np.sin(angle)],
@@ -19,10 +53,22 @@ def rotateY(verts, angle) :
                     [-np.sin(angle), 0, np.cos(angle)]])
     return np.matmul(verts, rot)
 
+# List of Vertices, Angle -> List of Vertices
+# Performs a rotation transformation around the X-axis
+# returns the result of the operation
+def rotateX(verts, angle) :
+    rot = np.array([[1, 0, 0],
+                    [0,np.cos(angle), -np.sin(angle)],
+                    [0,np.sin(angle), np.cos(angle)]])
+    return np.matmul(verts, rot)
+
 
 def run():
     # initialize pygame
     global vertexes
+    playing = False
+    done = False
+    wireframe = False
     pg.init()
 
     hh = 240
@@ -34,14 +80,31 @@ def run():
         # Iterate over events
         for ee in pg.event.get():
             if ee.type == pg.QUIT:
-                pg.quit()
+                done = True
             if ee.type ==pg.KEYDOWN:
                 if ee.key == pg.K_LEFT:
                     vertexes = rotateY(vertexes, np.pi/36)
                 elif ee.key == pg.K_RIGHT:
                     vertexes = rotateY(vertexes, -np.pi/36)
+                elif ee.key == pg.K_UP:
+                    vertexes = rotateX(vertexes, np.pi/36)
+                elif ee.key == pg.K_DOWN:
+                    vertexes = rotateX(vertexes, -np.pi/36)
+                elif ee.key == pg.K_X:
+                    wireframe = not wireframe
+                elif ee.key == pg.K_SPACE:
+                    playing = not playing
 
+        # State Decisions
+        if done:
+            pg.quit()
+            break # out of the main loop
+        
+        if playing:
+            vertexes = rotateY(vertexes, np.pi/100)
+            
 
+                    
         # Draw Screen
         # 1. World -> Cam
         # Camera is 4 units in front of (0, 0, 0)
@@ -60,6 +123,20 @@ def run():
         disp.fill((255,255,255))
         for pt in screen_coords.astype(np.int16):
             pg.draw.circle(disp, [0,0,255], pt, 2)
+
+        if wireframe:
+            for edge in edges:
+                p1 = screen_coords.astype(np.int16)[edge[0]]
+                p2 = screen_coords.astype(np.int16)[edge[1]]
+                pg.draw.line(disp, [0,0,0], p1, p2, 1)
+        else:
+            for face in faces:
+                face.updateZ()
+            import heapq
+            # sort a list of faces by face.zval
+            
+
+        
 
         pg.display.flip()
 
